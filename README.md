b4b825e8d960b23581b47f35754edef6cfd31457
lbb00 <love@onelong.org>
2023-02-01 17:00:13 +0800

    feat: add README.md

---
+++ README.md
@@ -0,0 +1,5 @@
+# 移除飞书文档水印
+
+## Install
+
+安装点这里: [Figma Css Better](https://github.com/lbb00/remove-feishu-watermark/raw/main/remove-feishu-watermark.user.js)
--- remove-feishu-watermark.user.js
+++ remove-feishu-watermark.user.js
@@ -5,6 +5,9 @@
 // @match        https://*.feishu.cn/*
 // @grant        GM_addStyle
 // @author       lbb00
+// @homepage    https://github.com/lbb00/remove-feishu-watermark
+// @updateURL   https://github.com/lbb00/remove-feishu-watermark/raw/main/remove-feishu-watermark.user.js
+// @downloadURL https://github.com/lbb00/remove-feishu-watermark/raw/main/remove-feishu-watermark.user.js
 // ==/UserScript==
 
 GM_addStyle('div[style|=z]:not([class]):not(:has(*)) {opacity: 0;}')
