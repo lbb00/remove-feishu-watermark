b76b8438f851f7461cb2a53196b455b8f7191d98
lbb <love@onelong.org>
2023-04-04 20:33:52 +0800

    fix: add match lark domain

--- remove-feishu-watermark.user.js
+++ remove-feishu-watermark.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
-// @name        移除飞书网页水印
+// @name        Remove lark watermark | 移除飞书网页水印
 // @description 移除飞书文档、工作台水印
-// @version     0.2.0
+// @version     0.2.1
 // @author      lbb00
 // @homepage    https://github.com/lbb00/remove-feishu-watermark
 // @updateURL   https://github.com/lbb00/remove-feishu-watermark/raw/main/remove-feishu-watermark.user.js
 // @downloadURL https://github.com/lbb00/remove-feishu-watermark/raw/main/remove-feishu-watermark.user.js
 // @match       https://*.feishu.cn/*
+// @match       https://*.larksuite.com/*
 // @run-at      document-start
 // @grant       GM_addStyle
 // ==/UserScript==
