88327439cf37a6041de8b3975d80e3609ce35fb6
lbb00 <mr@lbb00.com>
2023-04-26 17:56:27 +0800

    feat: replace opacity with background-image

--- remove-feishu-watermark.user.js
+++ remove-feishu-watermark.user.js
@@ -5,7 +5,7 @@
 // @description:zh-CN 移除飞书文档、工作台水印
 // @name:en           Remove watermarks of lark
 // @description:en    Remove watermarks from Lark documents and workspace.
-// @version           0.5.0
+// @version           0.5.1
 // @license           The Unlicense
 // @author            lbb00
 // @homepage          https://github.com/lbb00/remove-feishu-watermark
@@ -18,17 +18,15 @@
 // ==/UserScript==
 
 // global
-GM_addStyle('[class*="watermark"]{opacity: 0;}')
+GM_addStyle('[class*="watermark"]{background-image:  none !important:}')
 
 // 飞书文档
-GM_addStyle('.ssrWaterMark{opacity: 0;}')
-GM_addStyle('body>div>div>div>div[style*="position: fixed"]:not(:has(*)){opacity: 0;}')
+GM_addStyle('.ssrWaterMark{background-image:  none !important;}')
+GM_addStyle('body>div>div>div>div[style*="position: fixed"]:not(:has(*)){background-image:  none !important;}')
 
 // fixed for https://github.com/lbb00/remove-feishu-watermark/issues/3
-GM_addStyle('body>div[style*="position: fixed"]:not(:has(*)){opacity: 0;}') // for readonly
+GM_addStyle('body>div[style*="position: fixed"]:not(:has(*)){background-image: none !important;}') // for readonly
 
 // 工作台
-GM_addStyle('#watermark-cache-container{opacity: 0;}')
-GM_addStyle('body>div[style*="inset: 0px;"]:not(:has(*)){opacity: 0;}')
-
-
+GM_addStyle('#watermark-cache-container{background-image:  none !important;}')
+GM_addStyle('body>div[style*="inset: 0px;"]:not(:has(*)){background-image:  none !important}')
